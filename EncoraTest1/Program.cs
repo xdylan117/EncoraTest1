112937987aa251c3e3e35b5bca6f0ab09e82b5d4
Dylan Arroyo <dylan_arroyomejias@hotmail.com>
2021-07-23 21:44:00 -0600

    final

--- EncoraTest1/Program.cs
+++ EncoraTest1/Program.cs
@@ -4,6 +4,8 @@ namespace EncoraTest1
 {
     static class Program
     {
+        private static int index = 0;
+        private static string input;
         static void Main(string[] args)
         {
             //Add intro messages and awaiting for key to start.
@@ -17,10 +19,29 @@ namespace EncoraTest1
             {
                 //Starting a refresh exercise.
                 Console.Clear();
-                Console.WriteLine("Please enter a word: ");
+                Console.WriteLine("There are two options:");
+                Console.WriteLine("Press 1 to do a loop and continues until Escape key is pressed.");
+                Console.WriteLine("Press 2 to do a loop and prints the input until reach the end and start back at the beginning");
+                var menu = Console.ReadLine();
 
-                //Calling MakeLooper according user input.
-                Console.WriteLine(MakeLooper(Console.ReadLine()));
+                //Doing a menu depending on choice and returning if not valid choice.
+                switch (menu)
+                {
+                    case "1":
+                        cleanInput();
+                        do
+                        {
+                            Console.WriteLine(makeLooper(input));
+                        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+                        break;
+                    case "2":
+                        cleanInput();
+                        makeLooperAlt(input);
+                        Console.ReadKey();
+                        break;
+                    default:
+                        return;
+                }
 
                 //Reading loop choice.
                 Console.WriteLine("Do you know to try another phrase? (Y/N)");
@@ -29,10 +50,22 @@ namespace EncoraTest1
         }
 
         /// <summary>
+        /// Returning a string for each char of the text and restating index once reaches the end.
+        /// </summary>
+        /// <param name="text"></param>
+        static string makeLooper(string text)
+        {
+            //Getting the char according the index given in a continues loop
+            var result = text.Substring(index, 1);
+            index = text.Length - 1 ==  index ? 0 : ++index;
+            return result;
+        }
+
+        /// <summary>
         /// Returning a string for each char of the text until reaches end and starts back.
         /// </summary>
         /// <param name="text"></param>
-        static string MakeLooper(string text)
+        static string makeLooperAlt(string text)
         {
             //Validate if it's a valid text.
             if (text.Length > 0)
@@ -55,6 +88,13 @@ namespace EncoraTest1
             {
                 return "Please give a phrase or word to work.";
             }
+        }
+
+        static void cleanInput()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter a word: ");
+            input = Console.ReadLine();
         }
 
     }
